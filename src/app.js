4bb78fc85c5a43bd166310ff2576c7f76ea484d3
BJ Neilsen <bj.neilsen@gmail.com>
2019-02-16 13:53:10 -0500

    Wrap app in custom mui theme

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import { Provider } from 'react-redux'
+import { MuiThemeProvider } from '@material-ui/core/styles'
 import { store } from 'src/redux'
 import Routes from 'src/routes'
+import theme from 'src/theme'
 
 export default () => (
   <Provider store={store}>
-    <Routes />
+    <MuiThemeProvider theme={theme}>
+      <Routes />
+    </MuiThemeProvider>
   </Provider>
 )
