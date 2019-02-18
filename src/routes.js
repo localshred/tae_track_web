c95edd91bcacb691f55af04c69aae6541ca56105
BJ Neilsen <bj.neilsen@gmail.com>
2019-02-17 19:55:46 -0500

    Add NotFound route

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,23 @@
 import React from 'react'
-import { Router } from '@reach/router'
+import { Link, Router } from '@reach/router'
 import ContactList from 'src/pages/contact-list'
 import LessonList from 'src/pages/lesson-list'
 import StudentList from 'src/pages/student-list'
+import Typography from '@material-ui/core/Typography'
+
+const NotFound = props => (
+  <div>
+    <Typography variant='h4'>Page Not Found</Typography>
+    <Typography variant='body1'>
+      We don't know what to show you at {props.uri}, so you should probably{' '}
+      <Link to='/students'>Go Home</Link>.
+    </Typography>
+  </div>
+)
 
 export default () => (
   <Router>
+    <NotFound default />
     <ContactList path='/contacts' />
     <LessonList path='/lessons' />
     <StudentList path='/students' />
